fa2882824bde27829552534d666a45dfeee2c19a
Sven Werlen <sven.werlen@pm.me>
2020-04-27 22:47:30 -0400

    Class features

--- scripts/loaddata.js
+++ scripts/loaddata.js
@@ -7,28 +7,32 @@ async function pf1frLoadData() {
   console.log(`PF1-FR | Updating Data`);
   
   // load JSON data
-  const dClasses = await fetch("/data/classes.json").then(r => r.json()) // Load your JSON data
-  const dFeats   = await fetch("/data/feats.json").then(r => r.json()) // Load your JSON data
-  const dSpells  = await fetch("/data/spells.json").then(r => r.json()) // Load your JSON data
-  const dWeapons = await fetch("/data/weapons.json").then(r => r.json()) // Load your JSON data
-  const dArmors  = await fetch("/data/armors.json").then(r => r.json()) // Load your JSON data
-  const dMagic   = await fetch("/data/magic.json").then(r => r.json()) // Load your JSON data
+  const dClasses  = await fetch("/data/classes.json").then(r => r.json()) // Load your JSON data
+  const dFeats    = await fetch("/data/feats.json").then(r => r.json()) // Load your JSON data
+  const dFeatures = await fetch("/data/classfeatures.json").then(r => r.json()) // Load your JSON data
+  const dSpells   = await fetch("/data/spells.json").then(r => r.json()) // Load your JSON data
+  const dWeapons  = await fetch("/data/weapons.json").then(r => r.json()) // Load your JSON data
+  const dArmors   = await fetch("/data/armors.json").then(r => r.json()) // Load your JSON data
+  const dMagic    = await fetch("/data/magic.json").then(r => r.json()) // Load your JSON data
   
   // create compendiums
   let pClasses = await Compendium.create({label: "ImportClasses", entity: "Item"})
   let pFeats = await Compendium.create({label: "ImportFeats", entity: "Item"})
+  let pFeatures = await Compendium.create({label: "ImportFeatures", entity: "Item"})
   let pItems = await Compendium.create({label: "ImportItems", entity: "Item"})
   let pSpells = await Compendium.create({label: "ImportSpells", entity: "Item"})
   
   // retrieve compendiums
   const packClasses = game.packs.find(p => p.metadata.label === "ImportClasses"); 
-  const packFeats = game.packs.find(p => p.metadata.label === "ImportFeats"); 
+  const packFeats = game.packs.find(p => p.metadata.label === "ImportFeats");
+  const packFeatures = game.packs.find(p => p.metadata.label === "ImportFeatures"); 
   const packItems = game.packs.find(p => p.metadata.label === "ImportItems"); 
   const packSpells = game.packs.find(p => p.metadata.label === "ImportSpells"); 
   
   // import data
   await packClasses.createEntity(dClasses);
   await packFeats.createEntity(dFeats);
+  await packFeatures.createEntity(dFeatures);
   await packItems.createEntity(dWeapons);
   await packItems.createEntity(dArmors);
   await packItems.createEntity(dMagic);
@@ -94,6 +98,7 @@ async function pf1frLoadCharacter() {
   }
   
   const AB_MAP = { 'Force': "str", 'Dextérité': "dex", 'Constitution': "con", 'Intelligence': "int", 'Sagesse': "wis", 'Charisme': "cha" }
+  const SK_SPEC = { 'crf' : "int", 'pro' : "wis", 'prf' : "cha" }
   const SK_MAP = { 
     'Acrobaties': "acr", 'Estimation': "apr", 'Bluff': "blf", 'Escalade': "clm", 'Artisanat': "crf", 'Diplomatie': "dip", 'Sabotage': "dev",
     'Déguisement': "dis", 'Évasion': "esc", 'Vol': "fly", 'Dressage': "han", 'Premiers secours': "hea", 'Intimidation': "int", 'Connaissances (mystères)': "kar",
@@ -108,7 +113,14 @@ async function pf1frLoadCharacter() {
     cdata.data.abilities[AB_MAP[key]] = { 'value': c[key] }
   });
   pj['Compétences'].forEach(function(sk) {
-    cdata.data.skills[SK_MAP[sk['Nom']]] = { 'rank': sk['Rang'] }
+    let key = SK_MAP[sk['Nom']];
+    if(key in SK_SPEC) {
+      cdata.data.skills[key] = { 'value': 0, 'subSkills': {} }
+      cdata.data.skills[key]['subSkills'][key.concat('1')] = { 'name': '??', 'rank': sk['Rang'], 'ability': SK_SPEC[key] }
+    } else {
+      cdata.data.skills[key] = { 'rank': sk['Rang'] }
+    }
+    
   });
   
   
@@ -121,12 +133,21 @@ async function pf1frLoadCharacter() {
     // add class(es)
     const packClasses = game.packs.find(p => p.collection === "pf1-fr.classesfr");
     packClasses.getIndex().then(function(idx) {
-      console.log(idx);
       pj['Classes'].forEach(function(cl) {
         const cla = packClasses.index.find(c => c.name === cl['Nom']);
         packClasses.getEntity(cla._id).then(function(c) {
           c.data.data.levels = cl['Niveau']
           actor.createEmbeddedEntity("OwnedItem",c)
+        });
+      });
+    });
+    // add feat(s)
+    const packFeats = game.packs.find(p => p.collection === "pf1-fr.featsfr");
+    packFeats.getIndex().then(function(idx) {
+      pj['Dons'].forEach(function(feat) {
+        const fea = packFeats.index.find(f => f.name === feat['Nom']);
+        packFeats.getEntity(fea._id).then(function(f) {
+          actor.createEmbeddedEntity("OwnedItem",f)
         });
       });
     });
