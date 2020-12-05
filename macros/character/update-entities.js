da0e24d38b7b44b40b5ab5804eb7a0b78fdaa0b1
Sven Werlen <sven.werlen@pm.me>
2020-12-05 12:16:51 -0500

    Mise à jour des descriptions

--- macros/character/update-entities.js
+++ macros/character/update-entities.js
@@ -1,76 +1,95 @@
-Id: UN955S6O6dX0xzmM
-Name: Mettre à jour les aptitudes
+Id: s16XixMjKJcJYR7t
+Name: Mettre à jour les descriptions
 Icon: systems/pf1/icons/skills/blue_18.jpg
 ------------
 ///// INFORMATIONS
 //
-// Cette macro ajoute les aptitudes sur un PJ (basé sur ses classes) 
-// À utiliser typiquement après un passage de niveau ou à la création.
+// Cette macro met à jour les descriptions de tous les éléments du personnage 
+// (race, classe, dons, aptitudes, traits, sorts) à partir des versions des
+// compendiums de `pf1-fr`.
+// Ne met pas à jour l'équipement.
 //
-// Base : Foundry VTT (0.6.6)
-// Système : Pathfinder 1 (0.73.7)
+// Base : Foundry VTT (0.7.7)
+// Système : Pathfinder 1 (0.76.7)
 // Module(s) nécessaire(s) : -
 // Auteur(s) : Sven Werlen (Dorgendubal#3348)
 
+// 1 = remplacer par la description complète
+// 2 = remplacer par un lien vers l'item dans le compendium
+const CONFIG = 2
+
+
 // SCRIPT
 
-function getAbbr(name) {
-  if(name == "Barbare") { return "Brb" }
-  else if(name == "Prêtre combattant") { return "Prc" }
-  else if(name == "Archer-mage") { return "ArM" }
-  else if(name == "Champion occultiste") { return "Chp" }
-  else if(name == "Magus") { return "Mgs" }
-  else if(name == "Chaman") { return "Chm" }
-  else { return name.substring(0,3) }
-};
+async function updateItems(actor, items, compendium) {
+  const pack = game.packs.get(compendium)
+  const index = await pack.getIndex()
+  let updates = []
+  for(let e=0; e<items.length; e++) {
+    const item = items[e]
+    const found = index.find( idx => idx.name == item.name )
+    if( found ) { 
+      const el = await pack.getEntity(found._id)
+      let link = `@Compendium[${compendium}.${found._id}]{${found.name}}`
+      if( CONFIG == 1 ) {
+        if( el.type == "spell" ) {
+          updates.push( { _id: item._id, data: { shortDescription: getProperty(el.data, "data.shortDescription") } } ) 
+        } else {
+          updates.push( { _id: item._id, data: { description: { value: getProperty(el.data, "data.description.value") } } } ) 
+        }
+      } else {
+        if( el.type == "spell" ) {
+          updates.push( { _id: item._id, data: { shortDescription: link } } ) 
+        } else {
+          updates.push( { _id: item._id, data: { description: { value: link } } } ) 
+        }
+      }
+    }
+  }
+  return updates
+}
 
-const actors = MacrosPF1.getActors()
 
-if (actors.length != 1) { 
+let actors = MacrosPF1.getActors()
+
+if (actors.length < 1) { 
   ui.notifications.error("Veuillez choisir un personnage dans la scène!")
 } else {
   let actor = actors[0]
 
   Dialog.confirm({
-    title: "Mise à jour des aptitudes",
-    content: `Des aptitudes seront automatiquement ajoutées à <b>${actor.name}</b> en fonction de ses classes.`,
+    title: "Mise à jour des descriptions",
+    content: `Les descriptions de tous les éléments (race, classe, dons, aptitudes, traits et sorts) du personnage <b>${actor.name}</b> seront mise à jour à partir des compendiums. Tout changement manuel à ces descriptions sera perdu. <br/><br/><i>ATTENTION: un bogue non-identifié dans Foundry pourrait engendrer une fuite de mémoire suite à la mise à jour et nécessiter un redémarrage du serveur/application!!</i><br/><br/> Voulez-vous continuer?`,
       yes: function() {
 
-        let classes = []
-        let classfeatures = []
-        for(let i=0; i<actor.data.items.length; i++) {
-          const item = actor.data.items[i]
-          if(item.type == "feat" && item.data.featType == "classFeat") {
-            classfeatures.push(item.name)
-          } else if(item.type == "class") {
-            classes.push(item)
-          }
-        }
-        
-        if(classes.length > 0) {
-          const pack = game.packs.get("pf1-fr.classfeaturesfr");
-          pack.getIndex().then(index => {
-            let features = []
-            classes.forEach(c => {
-              let abbr = getAbbr(c.name)
-              let level = Number(c.data.level)
-              for(let lvl=1; lvl<=level; lvl++) {
-                for(const i in index) { 
-                  const f = index[i]
-                  if(f.name.startsWith(`${abbr} ${lvl<10?" ":""}${lvl} :`) && classfeatures.indexOf(f.name) < 0) {
-                    features.push(pack.getEntity(f._id))
-                  }
-                }
-              }
-            });
-            Promise.all(features).then(list => {
-              let toAdd = []
-              list.forEach( f => { if(f.data.data.tags[0].indexOf("De base") >= 0) { toAdd.push(f) } } );
-              actor.createEmbeddedEntity("OwnedItem", toAdd)
-              ui.notifications.info(`${toAdd.length} aptitude(s) ajoutées!`)
-            });
-          });
-        }
+        let promises = []
+        let items = actor.items.filter( i => i.type == "race" )
+        promises = promises.concat( updateItems(actor, items, "pf1-fr.racesfr") )
+
+        items = actor.items.filter( i => i.type == "class" )
+        promises = promises.concat( updateItems(actor, items, "pf1-fr.classesfr"))
+
+        items = actor.items.filter( i => i.type == "feat" && getProperty(i.data, "data.featType") == "feat" )
+        promises = promises.concat( updateItems(actor, items, "pf1-fr.featsfr"))
+
+        items = actor.items.filter( i => i.type == "feat" && getProperty(i.data, "data.featType") == "classFeat" )
+        promises = promises.concat( updateItems(actor, items, "pf1-fr.classfeaturesfr"))
+
+        items = actor.items.filter( i => i.type == "feat" && ( getProperty(i.data, "data.featType") == "trait" || getProperty(i.data, "data.featType") == "racial" ))
+        promises = promises.concat( updateItems(actor, items, "pf1-fr.traitsfr"))
+
+        items = actor.items.filter( i => i.type == "spell" )
+        promises = promises.concat( updateItems(actor, items, "pf1-fr.spellsfr"))
+
+        Promise.all(promises).then( p => {
+          let updates = []
+          p.forEach( el => { 
+            if( Array.isArray(el) ) updates = updates.concat(el)
+            else updates.push(el)
+          })
+          actor.updateEmbeddedEntity("OwnedItem", updates);
+          ui.notifications.info(`${updates.length} descriptions mises à jour!`)
+        });
       },
       no: () => {}
   });
---
+++ macros/character/update-features.js
@@ -0,0 +1,77 @@
+Id: UN955S6O6dX0xzmM
+Name: Mettre à jour les aptitudes
+Icon: systems/pf1/icons/skills/blue_18.jpg
+------------
+///// INFORMATIONS
+//
+// Cette macro ajoute les aptitudes sur un PJ (basé sur ses classes) 
+// À utiliser typiquement après un passage de niveau ou à la création.
+//
+// Base : Foundry VTT (0.6.6)
+// Système : Pathfinder 1 (0.73.7)
+// Module(s) nécessaire(s) : -
+// Auteur(s) : Sven Werlen (Dorgendubal#3348)
+
+// SCRIPT
+
+function getAbbr(name) {
+  if(name == "Barbare") { return "Brb" }
+  else if(name == "Prêtre combattant") { return "Prc" }
+  else if(name == "Archer-mage") { return "ArM" }
+  else if(name == "Champion occultiste") { return "Chp" }
+  else if(name == "Magus") { return "Mgs" }
+  else if(name == "Chaman") { return "Chm" }
+  else { return name.substring(0,3) }
+};
+
+const actors = MacrosPF1.getActors()
+
+if (actors.length != 1) { 
+  ui.notifications.error("Veuillez choisir un personnage dans la scène!")
+} else {
+  let actor = actors[0]
+
+  Dialog.confirm({
+    title: "Mise à jour des aptitudes",
+    content: `Des aptitudes seront automatiquement ajoutées à <b>${actor.name}</b> en fonction de ses classes.`,
+      yes: function() {
+
+        let classes = []
+        let classfeatures = []
+        for(let i=0; i<actor.data.items.length; i++) {
+          const item = actor.data.items[i]
+          if(item.type == "feat" && item.data.featType == "classFeat") {
+            classfeatures.push(item.name)
+          } else if(item.type == "class") {
+            classes.push(item)
+          }
+        }
+        
+        if(classes.length > 0) {
+          const pack = game.packs.get("pf1-fr.classfeaturesfr");
+          pack.getIndex().then(index => {
+            let features = []
+            classes.forEach(c => {
+              let abbr = getAbbr(c.name)
+              let level = Number(c.data.level)
+              for(let lvl=1; lvl<=level; lvl++) {
+                for(const i in index) { 
+                  const f = index[i]
+                  if(f.name.startsWith(`${abbr} ${lvl<10?" ":""}${lvl} :`) && classfeatures.indexOf(f.name) < 0) {
+                    features.push(pack.getEntity(f._id))
+                  }
+                }
+              }
+            });
+            Promise.all(features).then(list => {
+              let toAdd = []
+              list.forEach( f => { if(f.data.data.tags[0].indexOf("De base") >= 0) { toAdd.push(f) } } );
+              actor.createEmbeddedEntity("OwnedItem", toAdd)
+              ui.notifications.info(`${toAdd.length} aptitude(s) ajoutées!`)
+            });
+          });
+        }
+      },
+      no: () => {}
+  });
+}
